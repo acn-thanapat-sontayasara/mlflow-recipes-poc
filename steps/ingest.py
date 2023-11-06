4b092d3f126ebd7432c3f1e977395b727a15c8ca
thanapat.sontayasara <thanapat.sontayasara@accenture.com>
2023-11-07 00:20:44 +0700

    refactor(src): :hammer: Refactor read_spark_df_return_pandas

--- steps/ingest.py
+++ steps/ingest.py
@@ -1,10 +1,20 @@
+# import: standard
+import pandas
+
+# import: pyspark
+from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 from pyspark.sql.types import IntegerType
 
 
-def read_spark():
-    return (
+def read_spark_df_return_pandas(location: str, file_format: str) -> pandas.DataFrame:
+    
+    spark = SparkSession.getActiveSession()
+
+    spark_df = (
         spark.table("sample_dataset_db.titanic_feature_engineering")
-        .withColumn("Survived", col("Survived").cast(IntegerType()))
         .where(col("Survived").isNotNull())
+        .withColumn("Survived", col("Survived").cast(IntegerType()))
     )
+
+    return spark_df.toPandas()
